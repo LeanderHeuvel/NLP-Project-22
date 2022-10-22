2454f92e9b092210fec0dd28a20a138b7a56360c
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-10-22 19:57:05 +0200

    updated gitign

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from models import BERT_Model
 sarcastic_loader = dataloader.DataLoader(img_dir="archive/Sarcasm_Headlines_Dataset_v2.json", train_size = 0.6)
 # svm_model = SVM_classifier.SVM_Text_Model(sarcastic_loader)
 # svm_model.load_model("filename.joblib")
-bert_model = BERT_Model.BertModel(dataloader = sarcastic_loader)
+bert_model = BERT_Model.BertModel(dataloader = sarcastic_loader, epochs = 1)
+
 print("training...")
 bert_model.train()
-
+print("storing model")
+bert_model.store_model()
 print("evaluating...")
 print(bert_model.evaluate())
